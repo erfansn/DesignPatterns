9f95a5d0da7e12851c719326106765e94fae23f4
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2022-07-28 17:40:54 +0430

    Change singleton pattern example

--- src/main/kotlin/creational/Singleton.kt
+++ src/main/kotlin/creational/Singleton.kt
@@ -1,28 +1,75 @@
 package creational
 
-object Score {
-
-    private val numbers = mutableListOf<Int>()
-
-    fun add(number: Int) = also {
-        numbers += number
-    }
-
-    fun average() = numbers.average()
+interface AppDao {
+    fun insert(value: String)
+    fun update(index: Int, value: String)
+    fun delete(index: Int)
+    fun fetch(): List<String>
 }
 
-class App {
+class AppDaoImpl : AppDao {
 
-    companion object {
+    private val list = mutableListOf<String>()
 
-        @Volatile private var db: Any? = null
+    override fun insert(value: String) {
+        list += value
+    }
 
-        fun getDatabase(): Any {
-            return db ?: synchronized(this) {
-                Any().also {
-                    db = it
-                }
-            }
+    override fun update(index: Int, value: String) {
+        list[index] = value
+    }
+
+    override fun delete(index: Int) {
+        list.removeAt(index)
+    }
+
+    override fun fetch() = list
+}
+
+interface Database
+
+class OrmDatabase : Database {
+    val appDao = AppDaoImpl()
+}
+
+/** Create a singleton object with `object` modifier is thread-safe and its instantiation has lazy */
+object Application {
+
+    @Volatile
+    private var db: OrmDatabase? = null
+
+    fun getDatabase(): OrmDatabase {
+        return db ?: synchronized(this) {
+            OrmDatabase().also { db = it }
         }
     }
+
+    init {
+        println("App launched")
+    }
+}
+
+/**
+ * Pros:
+ * - You can be sure that a class has only a single instance.
+ * - You gain a global access point to that instance.
+ * - The singleton object is initialized only when it’s requested for the first time.
+ *
+ * Cons:
+ * - Violates the Single Responsibility Principle. The pattern solves two problems at the time.
+ * - The Singleton pattern can mask bad design, for instance, when the components of the program know too much about each other.
+ * - The pattern requires special treatment in a multithreaded environment so that multiple threads won’t create a singleton object several times.
+ * - It may be difficult to unit test the client code of the Singleton because many test frameworks rely on inheritance when producing mock objects.
+ * - Since the constructor of the singleton class is private and overriding static methods is impossible in most languages,
+ * you will need to think of a creative way to mock the singleton. Or just don’t write the tests. Or don’t use the Singleton pattern.
+ */
+fun main() {
+    val db = Application.getDatabase()
+    db.appDao.insert("singleton")
+
+    val db2 = Application.getDatabase()
+    println(db.appDao.fetch())
+
+    println(Application == Application)
+    println(db === db2)
 }
